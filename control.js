865649afa254070ca416c667da723891f678b6ad
Mariane <marianelove@sample.com>
2017-01-25 06:54:16 -1200

    first commit

---
+++ control.js
@@ -0,0 +1,20 @@
+
+$scope.addTodo = function () {
+			var newTodo = {
+				title: $scope.newTodo.trim(),
+				completed: false
+			};
+
+			if (!newTodo.title) {
+				return;
+			}
+
+			$scope.saving = true;
+			store.insert(newTodo)
+				.then(function success() {
+					$scope.newTodo = '';
+				})
+				.finally(function () {
+					$scope.saving = false;
+				});
+		};
